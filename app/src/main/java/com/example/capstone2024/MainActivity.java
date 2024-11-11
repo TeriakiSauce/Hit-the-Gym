7f09b20fdf0dabe15f8070d8d3ce30bf7bcfdb5f
Andrew Sahadeo <andrew.sahadeo2002@gmail.com>
2024-11-11 15:47:39 -0500

    Added default activity to the project

---
+++ app/src/main/java/com/example/capstone2024/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.capstone2024;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
